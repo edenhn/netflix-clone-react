461fc650e428e041d65e17a7a48065ddad024c8e
Student 35 <student35@Codes-MacBook-Air-59.local>
2024-01-01 18:33:14 -0500

    nextflix clone with tmdb api

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFF2TpDVymZtq4JNeTzEPbA1OvOlK-VbQ",
+    authDomain: "netflix-clone-cfa64.firebaseapp.com",
+    projectId: "netflix-clone-cfa64",
+    storageBucket: "netflix-clone-cfa64.appspot.com",
+    messagingSenderId: "8366737054",
+    appId: "1:8366737054:web:3bc0888ff8105551c9dd8f"
+  };
